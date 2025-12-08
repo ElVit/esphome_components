4dbab2567dce0b5c3d65934dfe1ce29bc9eb9fbb
ElVit <54866762+ElVit@users.noreply.github.com>
2025-12-08 10:22:15 +0100

    Update panasonic_heatpump_binary_sensor.cpp

--- components/panasonic_heatpump/binary_sensor/panasonic_heatpump_binary_sensor.cpp
+++ components/panasonic_heatpump/binary_sensor/panasonic_heatpump_binary_sensor.cpp
@@ -16,7 +16,7 @@ void PanasonicHeatpumpBinarySensor::publish_new_state(const std::vector<uint8_t>
   bool new_state;
   switch (this->id_) {
   case BinarySensorIds::CONF_UART_CLIENT_TIMED_OUT:
-    new_state = this->parent()->get_uart_client_timeout_exceeded();
+    new_state = this->parent_()->get_uart_client_timeout_exceeded();
     if (this->has_state() && this->state == new_state)
       return;
     break;
