2a2b121e45aacb308af57d0f69fbe40efa0b1348
ElVit <vitalij.salnikov@t-online.de>
2026-01-08 16:05:06 +0100

    fixed warning message in function 'read_request' (#13)

--- components/panasonic_heatpump/panasonic_heatpump.cpp
+++ components/panasonic_heatpump/panasonic_heatpump.cpp
@@ -230,10 +230,10 @@ void PanasonicHeatpumpComponent::read_request() {
       delay(10);  // NOLINT
       continue;
     }
-    // 4. byte shall be 0x10 or 0x21
-    if (this->request_message_.size() == 4 && byte_ != 0x10 && byte_ != 0x21) {
+    // 4. byte shall be 0x01, 0x10 or 0x21
+    if (this->request_message_.size() == 4 && byte_ != 0x01 && byte_ != 0x10 && byte_ != 0x21) {
       this->request_receiving_ = false;
-      ESP_LOGW(TAG, "Invalid request message: 0x%s. Expected last byte to be 0x10 or 0x21",
+      ESP_LOGW(TAG, "Invalid request message: 0x%s. Expected last byte to be 0x01, 0x10 or 0x21",
                PanasonicHelpers::byte_array_to_hex_string(this->request_message_, ',').c_str());
       delay(10);  // NOLINT
       continue;
